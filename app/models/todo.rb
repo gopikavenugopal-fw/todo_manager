5ec590fbb2c30de9333a2e949ae892f0faf536fd
Gopika Venugopal <gopika.venugopal@freshworks.com>
2021-03-16 23:51:26 +0530

    Sorted the record by date

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,7 @@
 class Todo< ActiveRecord::Base
 
+    def to_pleasant_string
+        is_completed=completed ? "[X]" : "[]"
+        "#{id}. #{todo_text} #{is_completed} on #{due_date.to_s(:short)}"
+    end
 end
